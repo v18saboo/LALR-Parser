3870a8964cdb2fe94a809fb17a462aa4ea7fee7a
Varun Saboo <v18saboo@gmail.com>
2015-10-18 18:18:06 +0530

    Generating all Item Sets. But stuck in infinite loop

--- LALR Parser.py
+++ LALR Parser.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import copy
-
+itemset_num=-1
+list_itemsets=list()
 
 def getProductions(productions,states):
 	file=open("grammar.txt","r")
@@ -65,30 +66,77 @@ def productionsWithLHS(element,productions):
 
 def closure(itemset,states,productions,lookahead):
 	#print(itemset)
-	if(states.__contains__(itemset[-1][1][1])):
-		parent_production=itemset[-1]
-		new = productionsWithLHS(itemset[-1][1][1],productions)
-		for temp in new:
-			temp[1]="." + temp[1]
-			if temp not in itemset:
-				lookahead[str(temp)]=lookahead[str(parent_production)]
-				itemset.append(temp)
-				closure(itemset,states,productions,lookahead)
+	rhs=itemset[-1][1]
+	try:
+		next_state=rhs[rhs.index(".")+1]
+		if(states.__contains__(next_state)):
+			parent_production=itemset[-1]
+			new = productionsWithLHS(next_state,productions)
+			for temp in new:
+				temp[1]="." + temp[1]
+				if temp not in itemset:
+					lookahead[str(temp)]=lookahead[str(parent_production)]
+					itemset.append(temp)
+					closure(itemset,states,productions,lookahead)
+	except IndexError:
+		pass				
+
+def swap(string,i):
+	j=i+1
+	c=list(string)
+	c[i],c[j]=c[j],c[i]
+	return ''.join(c)
 
 #Only generating ItemSet0 so far.			
-def generateItemSets(productions,states):
-	prod=copy.deepcopy(productions)
+def generateItemSets(productions,states,prod):
+	global itemset_num
+	itemset_num+=1
 	itemset=list()
 	lookahead=dict()
-	p=prod[0]
-	p[1]="." + p[1]
-	itemset.append(p)
-	lookahead[str(p)]="$"
-	closure(itemset,states,productions,lookahead)
-	print("ItemSet0")
-	print(itemset)
-	print("LookAhead")
-	print(lookahead)
+	flag=False
+	for p in prod:
+		if(not p[1].__contains__(".")):
+			p[1]="." + p[1]
+			flag=True
+		elif(p[1][-1]!="."):
+			index_of_dot=p[1].index(".")
+			p[1]=swap(p[1],index_of_dot)
+			flag=True	
+		itemset.append(p)
+		lookahead[str(p)]="$"
+	if(flag):	
+		closure(itemset,states,productions,lookahead)
+	print("ItemSet",itemset_num)
+	print(itemset,"\n")
+	if(itemset not in list_itemsets):
+		print("Above Itemset is Not in List Of All ITEMSETS")
+		list_itemsets.append(itemset)
+		if(itemset_num==9):
+			print(list_itemsets)
+			quit()
+	else:
+		print("Already Present in the itemset")	
+	afterDot=list()
+	# Key -> terminal/non terminal after the dot. 
+	# Value -> List of all productions which have key after the dot. Value forms the base of the itemset on transition on Key.
+	rhs_afterDot=dict() 
+	for i in itemset:
+		if (i[1][-1]=="."):
+			continue
+		next=i[1].index(".")+1
+		if not afterDot.__contains__(i[1][next]):
+			afterDot.append(i[1][next])
+			rhs_afterDot[i[1][next]]=list()
+		rhs_afterDot[i[1][next]].append(i)	
+	print(afterDot)	
+	print(rhs_afterDot)	
+	for var in afterDot:
+		#Make a list of those. Move the dot to the right by one. Find closure of that itemset.
+		new_itemset_productions = rhs_afterDot[var]
+		generateItemSets(productions,states,new_itemset_productions)
+
+	#print("LookAhead")
+	#print(lookahead)
 
 def main():
 	productions=list()
@@ -101,7 +149,7 @@ def main():
 	'''print(productions)
 	for key,value in first.items():
 		print("First(",key,") = ",str(value),sep='')'''
-	generateItemSets(productions,states)	
+	generateItemSets(productions,states,[productions[0]])	
 
 
 if(__name__=="__main__"):
